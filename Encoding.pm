dce408a21591fc576cd7754ae0bf374d0740e19d
cygx <cygx@cpan.org>
2016-10-09 23:04:30 +0200

    more encoding work

--- Encoding.pm
+++ Encoding.pm
@@ -1,6 +1,7 @@
+no precompilation;
 use nqp;
 
-sub is-unique($_) {
+sub has-unique-decomposition($_) {
     .NFC == .NFD && [==] .NFC.map: {
         nqp::getuniprop_int($_, BEGIN nqp::unipropcode('ccc'));
     }
@@ -10,42 +11,64 @@ my role Encoding { ... }
 
 my role Encoding::Decoder {}
 
-my class Encoding::Decoder::Generic { ... }
+my role Encoding::Decoder::Generic8[$] { ... }
+my role Encoding::Decoder::Generic16[$] { ... }
+my role Encoding::Decoder::Generic32[$] { ... }
 
-my role Encoding {
-    method decoder(*%options --> Encoding::Decoder:D) {
-        Encoding::Decoder::Generic.new(self);
+my role Encoding[Int:D \UNIT-BITS] {
+    method unit-bits(--> Int:D) { UNIT-BITS }
+
+    method decoder(--> Encoding::Decoder:D) {
+        given UNIT-BITS {
+            when 8 { Encoding::Decoder::Generic8[self].new(|%_) }
+            when 16 { Encoding::Decoder::Generic16[self].new(|%_) }
+            when 32 { Encoding::Decoder::Generic32[self].new(|%_) }
+        }
     }
 }
 
-my class Encoding::Decoder::Generic does Encoding::Decoder {
-    has $.encoding;
+my role Encoding::Decoder::Generic8[Encoding \ENC] does Encoding::Decoder {
+    has @.bytes = buf8.new;
+    has @.separators;
 
-    method new(Encoding $encoding) {
-        self.bless(:$encoding);
+    method encoding { ENC }
+
+    method new(:$sep = ("\n", "\r", "\r\n")) {
+        my \decoder = self.bless;
+        decoder.set-line-separators($sep.list);
+        decoder;
     }
 
     method set-line-separators(@seps --> Nil) {
-        @seps.map: {
+        @!separators = @seps.map: {
             when Str {
-                warn qq:to/END/ unless is-unique $_;
+                if .&has-unique-decomposition { ENC.encode-codes(.NFC, |%_) }
+                else {
+                    warn qq:to/END/;
     Separator {.perl} has ambiguous codepoint decomposition - falling back to
     NFC and NFD variants.
 
     To silence the warning, either provide manual decompositions as Uni instead
     of Str or use &split.
     END
-                # encode .NFC/.NFD
-                !!!
+                    slip ENC.encode-codes(.NFC, |%_),
+                         ENC.encode-codes(.NFD, |%_);
+                }
             }
-            when Uni { !!! }
+            when Uni { ENC.encode-codes($_, |%_) }
             when blob8 { $_ }
             default { die "cannot use {.perl} as separator" }
         }
     }
-
 }
 
-my role Encoding::UTF8 does Encoding {}
+my role Encoding::Decoder::Generic16[$] does Encoding::Decoder {}
+my role Encoding::Decoder::Generic32[$] does Encoding::Decoder {}
+
+my class Encoding::UTF8 does Encoding[8] {
+    method encode-codes(Uni:D $to-encode --> blob8:D) {
+        !!!
+    }
+}
 
 sub EXPORT { BEGIN Map.new((Encoding => Encoding)) }
--- NewIO.pm
+++ NewIO.pm
@@ -1,3 +1,4 @@
+no precompilation;
 use Encoding;
 use nqp;
 

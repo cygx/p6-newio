f6e2423f9fd5f5cb111b1c3e16b83b1349d4ebd2
cygx <cygx@cpan.org>
2016-10-10 20:15:35 +0200

    encoding refactor

--- Encoding.pm
+++ Encoding.pm
@@ -15,14 +15,39 @@ my role Encoding::Decoder::Generic8[$] { ... }
 my role Encoding::Decoder::Generic16[$] { ... }
 my role Encoding::Decoder::Generic32[$] { ... }
 
-my role Encoding[Int:D \UNIT-BITS] {
-    method unit-bits(--> Int:D) { UNIT-BITS }
+my role Encoding[Int:D \UNIT-BITS, Int:D \MAX-UNITS = 32 div UNIT-BITS] {
+    my \UNIT-BYTES = UNIT-BITS div 8;
+
+    method units-for-codepoint(uint32 --> Int:D) { ... }
+
+    method bytes-for-uni(Uni:D \uni --> Int:D) {
+        uni.map({ self.units-for-code($_) }).sum * UNIT-BYTES;
+    }
+
+    method encode-codepoint(uint32 \cp, Buf \buf, int $offset is rw --> Nil) {
+        ...
+    }
+
+    method encode-uni(Uni:D \uni --> blob8:D) {
+        my \buf = buf8.allocate(uni.elems * MAX-UNITS);
+        my int $i = 0;
+        self.encode-codepoint($_, buf, $i) for uni;
+        buf.reallocate($i);
+    }
+
+    proto method encode-str(Str:D \str --> blob8:D) { self.encode-uni({*}, |%_) }
+    multi method encode-str(Str:D \str, :$NFC!  --> blob8:D) { str.NFC }
+    multi method encode-str(Str:D \str, :$NFD!  --> blob8:D) { str.NFD }
+    multi method encode-str(Str:D \str, :$NFKC! --> blob8:D) { str.NFKC }
+    multi method encode-str(Str:D \str, :$NFKD! --> blob8:D) { str.NFKD }
+    multi method encode-str(Str:D \str          --> blob8:D) { str.NFC }
 
     method decoder(--> Encoding::Decoder:D) {
         given UNIT-BITS {
             when 8 { Encoding::Decoder::Generic8[self].new(|%_) }
             when 16 { Encoding::Decoder::Generic16[self].new(|%_) }
             when 32 { Encoding::Decoder::Generic32[self].new(|%_) }
+            default { !!! }
         }
     }
 }
@@ -79,36 +104,42 @@ my role Encoding::Decoder::Generic16[$] does Encoding::Decoder {}
 my role Encoding::Decoder::Generic32[$] does Encoding::Decoder {}
 
 my class Encoding::UTF8 does Encoding[8] {
-    method encode-codes(Uni:D \uni --> blob8:D) {
-        my \buf = buf8.allocate(uni.elems * 4);
-        my int $i = 0;
-        for uni {
-            when $_ <= 0x7F {
-                nqp::bindpos_i(buf, $i++, $_);
-            }
+    method units-for-codepoint(uint32 $_ --> Int:D) {
+        when $_ <= 0x7F     { 1 }
+        when $_ <= 0x7FF    { 2 }
+        when $_ <= 0xFFFF   { 3 }
+        when $_ <= 0x1FFFFF { 4 }
+        default { !!! }
+    }
 
-            when $_ <= 0x7FF {
-                nqp::bindpos_i(buf, $i++, 0xC0 +| ($_ +> 6));
-                nqp::bindpos_i(buf, $i++, 0x80 +| ($_ +& 0x3F));
-            }
-
-            when $_ <= 0xFFFF {
-                nqp::bindpos_i(buf, $i++, 0xE0 +|  ($_ +> 12));
-                nqp::bindpos_i(buf, $i++, 0x80 +| (($_ +>  6) +& 0x3F));
-                nqp::bindpos_i(buf, $i++, 0x80 +| ( $_        +& 0x3F));
-            }
-
-            when $_ <= 0x1FFFFF {
-                nqp::bindpos_i(buf, $i++, 0xF0 +|  ($_ +> 18));
-                nqp::bindpos_i(buf, $i++, 0x80 +| (($_ +> 12) +& 0x3F));
-                nqp::bindpos_i(buf, $i++, 0x80 +| (($_ +>  6) +& 0x3F));
-                nqp::bindpos_i(buf, $i++, 0x80 +| ( $_        +& 0x3F));
-            }
-
-            default { !!! }
+    method encode-codepoint(uint32 $_, Buf \buf, int $offset is rw --> Nil) {
+        when $_ <= 0x7F {
+            nqp::bindpos_i(buf, $offset, $_);
+            $offset = $offset + 1;
         }
 
-        buf.reallocate($i);
+        when $_ <= 0x7FF {
+            nqp::bindpos_i(buf, $offset,     0xC0 +| ($_ +> 6));
+            nqp::bindpos_i(buf, $offset + 1, 0x80 +| ($_ +& 0x3F));
+            $offset = $offset + 2;
+        }
+
+        when $_ <= 0xFFFF {
+            nqp::bindpos_i(buf, $offset,     0xE0 +|  ($_ +> 12));
+            nqp::bindpos_i(buf, $offset + 1, 0x80 +| (($_ +>  6) +& 0x3F));
+            nqp::bindpos_i(buf, $offset + 2, 0x80 +| ( $_        +& 0x3F));
+            $offset = $offset + 3;
+        }
+
+        when $_ <= 0x1FFFFF {
+            nqp::bindpos_i(buf, $offset,     0xF0 +|  ($_ +> 18));
+            nqp::bindpos_i(buf, $offset + 1, 0x80 +| (($_ +> 12) +& 0x3F));
+            nqp::bindpos_i(buf, $offset + 2, 0x80 +| (($_ +>  6) +& 0x3F));
+            nqp::bindpos_i(buf, $offset + 3, 0x80 +| ( $_        +& 0x3F));
+            $offset = $offset + 4;
+        }
+
+        default { !!! }
     }
 }
 

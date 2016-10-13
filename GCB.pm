53f02f7b2812aab699610183a1b3d0012898efa8
cygx <cygx@cpan.org>
2016-10-13 03:17:07 +0200

    update GCB.pm

--- GCB.pm
+++ GCB.pm
@@ -1,70 +1,117 @@
+# Copyright 2016 cygx <cygx@cpan.org>
+# Distributed under the Boost Software License, Version 1.0
+
 use nqp;
 
-unit module GCB;
+my class GCB is export {
+    my constant CODE = nqp::unipropcode('gcb');
 
-enum Property <
-    Other
-    Control CR LF
-    L V T LV LVT
-    Prepend Extend SpacingMark ZWJ Glue_After_Zwj
-    E_Base E_Base_GAZ E_Modifier Regional_Indicator
->;
+    sub prop(\name) { nqp::unipvalcode(CODE, name) }
+    sub getprop(\cp) { nqp::getuniprop_int(cp, CODE) }
 
-my constant PCOUNT = +Property::;
-my constant TABLE = do {
-    my @table[PCOUNT;PCOUNT] = (True xx PCOUNT) xx PCOUNT;
-    @table[CR;LF] = False;
-    @table[L;$_] = False for L, V, LV, LVT;
-    @table[$_;T] = False for V, T, LV, LVT;
-    @table[$_;V] = False for V, LV;
-    @table[$_;Extend] = False for Property::.values;
-    @table[$_;ZWJ] = False for Property::.values;
-    @table[$_;SpacingMark] = False for Property::.values;
-    @table[Prepend;$_] = False for Property::.values;
-    @table[$_;E_Modifier] = False for E_Base, E_Base_GAZ, Extend;
-    @table[ZWJ;$_] for Glue_After_Zwj, E_Base_GAZ;
-    @table[Regional_Indicator;Regional_Indicator] = False;
-    @table;
-}
+    my enum _ (
+        Other               => prop('Other'),
+        Control             => prop('Control'),
+        CR                  => prop('CR'),
+        LF                  => prop('LF'),
+        L                   => prop('L'),
+        V                   => prop('V'),
+        T                   => prop('T'),
+        LV                  => prop('LV'),
+        LVT                 => prop('LVT'),
+        Prepend             => prop('Prepend'),
+        Extend              => prop('Extend'),
+        SpacingMark         => prop('SpacingMark'),
+        ZWJ                 => prop('ZWJ'),
+        Glue_After_Zwj      => prop('Glue_After_Zwj'),
+        E_Base              => prop('E_Base'),
+        E_Base_GAZ          => prop('E_Base_GAZ'),
+        E_Modifier          => prop('E_Modifier'),
+        Regional_Indicator  => prop('Regional_Indicator'),
+    );
 
-our sub get-property(uint32 \code) {
-    Property::{nqp::getuniprop_str(code, BEGIN nqp::unipropcode('gcb'))};
-}
+    my constant COUNT = +_::;
 
-our sub is-break(uint32 \a, uint32 \b) {
-    TABLE[get-property(a);get-property(b)];
-}
+    sub idx(\a, \b) { a * COUNT + b }
 
-our sub is-potential-break(uint32 \a, uint32 \b) {
-    my \pa = get-property(a);
-    my \pb = get-property(b);
-    TABLE[pa;pb] || (pa == Extend && pb == E_Modifier)
-                 || (pa == pb == Regional_Indicator);
-}
+    my constant ALWAYS = (for ^COUNT -> \a {
+        slip (for ^COUNT -> \b {
+            given \(a, b) {
+                when :($ where CR,
+                       $ where LF) { False }
 
-our sub clusters(Uni \uni) {
-    return uni if uni.elems < 2;
-    my $emoji = False;
-    my $ri = False;
-    my int $i = 0;
-    my int $j = 0;
-    gather {
-        while ++$j < uni.elems {
-            my \pa = get-property(uni[$j-1]);
-            my \pb = get-property(uni[$j]);
-            if TABLE[pa;pb]
-                    || (!$emoji && (pa == Extend && pb == E_Modifier))
-                    || ($ri && pa == pb == Regional_Indicator) {
-                take Uni.new(uni[$i..^$j]);
-                $i = $j;
-                $emoji = False;
-                $ri = False;
+                when :($ where Control|CR|LF,
+                       $) { True }
+
+                when :($,
+                       $ where Control|CR|LF) { True }
+
+                when :($ where L,
+                       $ where L|V|LV|LVT) { False }
+
+                when :($ where LV|V,
+                       $ where V|T) { False }
+
+                when :($ where LVT|T,
+                       $ where T) { False }
+
+                when :($,
+                       $ where Extend|ZWJ) { False }
+
+                when :($,
+                       $ where SpacingMark) { False }
+
+                when :($ where Prepend,
+                       $) { False }
+
+                when :($ where E_Base|E_Base_GAZ|Extend,
+                       $ where E_Modifier) { False }
+
+                when :($ where ZWJ,
+                       $ where Glue_After_Zwj|E_Base_GAZ) { False }
+
+                when :($ where Regional_Indicator,
+                       $ where Regional_Indicator) { False }
+
+                default { True }
             }
-            else {
-                $emoji = True if pa == any(E_Base, E_Base_GAZ);
-                $ri = True if pa == pb == Regional_Indicator;
+        });
+    });
+
+    my constant MAYBE = do given [ALWAYS] {
+        .[idx Extend, E_Modifier] = True;
+        .[idx Regional_Indicator, Regional_Indicator] = True;
+        list |$_;
+    }
+
+    method always(uint32 \a, uint32 \b) { ALWAYS[idx getprop(a), getprop(b)] }
+    method maybe(uint32 \a, uint32 \b) { MAYBE[idx getprop(a), getprop(b)] }
+
+    proto method clusters(|) {*}
+    multi method clusters(Uni \uni where uni.elems < 2) { uni }
+    multi method clusters(Uni \uni) {
+        my $emoji = False;
+        my $regio = False;
+        my int $i = 0;
+        my int $j = 0;
+        gather {
+            while ($j = $j + 1) < uni.elems {
+                my \pa = getprop(uni[$j-1]);
+                my \pb = getprop(uni[$j]);
+                if ALWAYS[idx pa, pb]
+                        || (!$emoji && pa == Extend && pb == E_Modifier)
+                        || ( $regio && pa == pb == Regional_Indicator) {
+                    take Uni.new(uni[$i..^$j]);
+                    $i = $j;
+                    $emoji = False;
+                    $regio = False;
+                }
+                else {
+                    $emoji = True if pa == E_Base|E_Base_GAZ;
+                    $regio = True if pa == pb == Regional_Indicator;
+                }
             }
+            take Uni.new(uni[$i..^$j]);
         }
-        take Uni.new(uni[$i..^$j]);
     }
 }
